0e793a5b8c6644243ec532858725daaaca6dd8aa
karmfield3 <karmfield3@gatech.edu>
2022-11-27 23:36:14 -0500

    fixed influx

--- influx_db/client.py
+++ influx_db/client.py
@@ -1,73 +1,56 @@
+from datetime import datetime, timedelta
 from datetime import datetime
-import os
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client import InfluxDBClient, WriteOptions
 from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
 from datetime import datetime
-import csv
 import pandas as pd
-import concurrent
-import _thread
 
 token = "xDfhQrrKUo7vZAZgGEk3lZCLCJcQ2oC8l7GAxHBw6kw-Ghh8ai_D-s8hgJkdL7TH9uakMKRi2U0jJw8oQOzWuQ=="
 org = "gt"
-bucket = "new1"
+bucket = "stock_bucket"
+url="http://localhost:8086"
 
-class InfluxClient:
-    def __init__(self,token,org,bucket): 
-        self._org=org 
-        self._bucket = bucket
-        self._client = InfluxDBClient(url="http://localhost:8086", token=token)
-        # self.write_api = self.client.write_api(
-        #     write_options=WriteOptions(write_type=WriteType.batching, batch_size=50_000, flush_interval=10_000))
+client = InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+)
+write_api = client.write_api(write_options=WriteOptions(SYNCHRONOUS, batch_size=10_000, flush_interval=10_000, jitter_interval=2_000, retry_interval=5_000, max_retries=5, max_retry_delay=30_000, exponential_base=2))
 
-    def write_data(self,data,write_option=ASYNCHRONOUS):
-      write_api = self._client.write_api(write_option)
-      start_time = datetime.now()
-      write_api.write(self._bucket, self._org, data, data_frame_measurement_name='Symbol')
-      total_time = datetime.now() - start_time
-      print(total_time)
+def load_data():
+  df = pd.read_csv("../stock_csv/ALLE.csv")
+  # print(df)                            
+  start_time = datetime.now()
+  write_api.write(bucket=bucket, org=org, record=df, data_frame_measurement_name='Symbol', data_frame_tag_columns=['Symbol'])
+  end_time = datetime.now()
+  print("Load Time: ")
+  print(end_time-start_time)
 
-    def query_data(self,query):
-      query_api = self._client.query_api()
-      result = query_api.query(org=self._org, query=query)
-      results = []
-      for table in result:
-        for record in table.records:
+
+def query_data(query):
+  query_api = client.query_api()
+  start_time = datetime.now()
+  result = query_api.query(org=org, query=query)
+  results = []
+  for table in result:
+      for record in table.records:
           results.append((record.get_value(), record.get_field()))
-      return results 
-    
+  print(results)
+  end_time = datetime.now()
+  print("Query Time: ")
+  print(end_time-start_time)
+  return results 
 
-def multithread(df, client):
-  client.write_data(df)
-  
-def write(data, client):
-  client.write(data)
+
 
 def main():
-  # IC = InfluxClient(token,org,bucket)
-  
-  # IC2 = InfluxClient(token,org,bucket)
-  _now = datetime.utcnow()
-  df = pd.read_csv('../stock_csv/ALLE.csv', header=0)
-  df = df.set_index("Date")
+  load_data()
 
-  client = InfluxDBClient(url="http://localhost:8086", token=token)
-  _thread.start_new_thread(write, (df, client))
-  
-
-  
-  
-  # IC.write_data(df)
-  # IC2.write_data(df)
-  # num_clients = 2
-  # multithread(df, num_clients)
-  # _thread.start_new_thread(IC.write_data, (df,))
-  # _thread.start_new_thread(IC2.write_data, (df,))
-  # load_data(df, num_clients)
-
-  # multi_thread(IC, csv_reader, csv_rows, 5)
+  query1 = 'from(bucket: "stock_bucket")\
+  |> range(start: 1633124983)\
+  |> filter(fn: (r) => r._field == "High")'
+  query_data(query1)
 
 
 if __name__ == "__main__":
-  main()  
-
+  main()
